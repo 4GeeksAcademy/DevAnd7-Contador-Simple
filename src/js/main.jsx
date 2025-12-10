0301f66ee75541b550b1805e1d26d41e06bca4cc
Andri Arrieta <andriarrieta7@gmail.com>
2025-12-10 19:57:25 +0000

    contador

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,31 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-//Bootstrap
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap"
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-// index.css
+
 import '../styles/index.css'
 
-// components
+
 import Card from './components/Card';
 import Cardreloj from './components/Cardreloj';
 
 let numero = 0;
 
 setInterval(() => {
-  // Convertimos el número en string, lo rellenamos con ceros hasta 6 dígitos
+
   const digitos = numero.toString().padStart(6, "0").split("");
 
   ReactDOM.createRoot(document.getElementById('root')).render(
     <>
       <div className='d-flex justify-content-center'>
-        {/* Caja para el reloj */}
+       
         <Cardreloj />
 
-        {/* 6 cajas para los dígitos */}
+        
         {digitos.map((d, i) => (
           <Card key={i} num={d} />
         ))}
